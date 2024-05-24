c86eb81181da8d4fef413d0012a7076c124ebfba
mnsuhairil <94582522+mnsuhairil@users.noreply.github.com>
2024-05-24 16:02:45 +0800

    add boolean showaddbutton

--- lib/component/appbar/app_bar.dart
+++ lib/component/appbar/app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfoliomanagementapp/add_project_page.dart';
 
 // import 'package:portfoliomanagementapp/resources/app_colors.dart';
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -6,36 +7,47 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   final void Function() onMenuPressed;
   final void Function(String newTitle)
       setTitle; // Callback function to update the title
+  final bool showAddButton; // Flag to control the add button visibility
 
   const MyAppBar({
     Key? key,
     required this.title,
     required this.onMenuPressed,
     required this.setTitle, // Initialize the callback function
+    required this.showAddButton, // Add showAddButton parameter
   }) : super(key: key);
 
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      leading: showAddButton
+          ? IconButton(
+              icon: const Icon(
+                Icons.add,
+                color: Color(0xFF8999D7),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const AddProjectPage()), // Navigate to AddProjectPage
+                );
+              },
+            )
+          : null, // Conditional rendering based on showAddButton
       title: Text(title),
       titleTextStyle: const TextStyle(
         color: Color(0xFFFFFDFF),
         fontSize: 20,
         fontWeight: FontWeight.bold,
       ),
-      actions: [
-        // IconButton(
-        //   icon: const Icon(
-        //     Icons.menu,
-        //     color: Color(0xFF8999D7),
-        //   ),
-        //   onPressed: onMenuPressed,
-        // ),
+      actions: const [
         Padding(
-          padding: const EdgeInsets.all(5.0),
+          padding: EdgeInsets.all(5.0),
           child: CircleAvatar(
             radius: 35,
             backgroundImage: NetworkImage(
